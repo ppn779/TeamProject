d8c0cd667dc99ec74516e9437a19841feb0cc53e
K-Hoon <38878244+K-Hoon@users.noreply.github.com>
2018-05-10 14:06:08 +0900

    1

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : TacticsMove
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!moving)
+        {
+            FindSelectableTiles();
+            CheckMouse();
+        }
+    }
+
+    void CheckMouse()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.tag == "Tile")
+                {
+                    Tile t = hit.collider.GetComponent<Tile>();
+
+                    if (t.selectable)
+                    {
+                        MoveToTile(t);
+                    }
+                }
+            }
+        }
+    }
+
+}
