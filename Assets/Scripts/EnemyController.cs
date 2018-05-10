b564ad5809c037ed0dccc76a3435e5a70c5f7d23
K-Hoon <38878244+K-Hoon@users.noreply.github.com>
2018-05-10 16:17:40 +0900

    Monster

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class EnemyController : MonoBehaviour
+{
+    public float lookRadius = 10f;
+
+    Transform target;
+    NavMeshAgent nvAgent;
+
+
+    private bool isDie = false;
+
+    private int hp = 100;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            nvAgent.SetDestination(target.position);
+
+            if (distance <= nvAgent.stoppingDistance)
+            {
+                // Attack the target
+                FaceTarget();
+            }
+        }
+    }
+
+
+    // 타겟 바라보기
+    void FaceTarget()
+    {
+        Vector3 dir = (target.position - transform.position).normalized;
+        Quaternion lookRot = Quaternion.LookRotation(new Vector3(dir.x, 0, dir.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRot, Time.deltaTime * 5f);
+    }
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
---
+++ Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
