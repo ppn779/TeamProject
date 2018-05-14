7b00b90e29ff1172b768759c5d455a624eecf97e
K-Hoon <38878244+K-Hoon@users.noreply.github.com>
2018-05-14 21:02:59 +0900

    0514
    EnemyManager
    EnemyControoler
    EnemyHealth

--- Assets/Scripts/CharactersStatusMng.cs
+++ Assets/Scripts/CharactersStatusMng.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CharactersStatusMng : MonoBehaviour {
+public class CharactersStatusMng : MonoBehaviour
+{
     private Player player;
     private Enemy enemy;
+
+    public Text playerHP;
     public Text enemyHP;
 
-    int eHP=100;//실험용
+    public static bool isPlayerAlive = true;
 
-	// Use this for initialization
-	void Start () {
+    public int pHP = 100;
+    int eHP = 100;//실험용
+
+    // Use this for initialization
+    void Start()
+    {
         player = FindObjectOfType<Player>();
         enemy = FindObjectOfType<Enemy>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0)&&enemy.isPlayerNearby)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && enemy.isPlayerNearby)
         {
-            eHP= enemy.beAttacked(player.Attack());
+            eHP = enemy.beAttacked(player.Attack());
         }
 
         enemyHP.text = "EnemyHP = " + eHP;
+
+        if(pHP <= 0)
+        {
+            isPlayerAlive = false;
+        }
     }
 }
