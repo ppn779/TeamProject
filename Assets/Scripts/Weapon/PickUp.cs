b71f824c9bf37da36a9ace9ba3605466d2f5c992
K-Hoon <38878244+K-Hoon@users.noreply.github.com>
2018-05-16 23:36:19 +0900

    merge

--- Assets/Scripts/Weapon/PickUp.cs
+++ Assets/Scripts/Weapon/PickUp.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour {
+    private Transform tr = null;
     private Inventory inven = null;
+    private Equipment equipment = null;
 	private void Start () {
-        inven = this.transform.GetComponent<Inventory>();
-        if (inven == null) { this.transform.gameObject.AddComponent<Inventory>(); }
+        tr = this.transform;
+        inven = tr.GetComponent<Inventory>();
+        if (inven == null) { tr.gameObject.AddComponent<Inventory>(); }
+        equipment = tr.GetComponent<Equipment>();
+        if (equipment == null) { tr.gameObject.AddComponent<Equipment>(); }
 	}
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Weapon"))
         {
             Debug.Log(collision.gameObject.name + "아이템을 주웠다");
-            inven.AddItem(collision.gameObject);
+            equipment.Equip(collision.gameObject);
+            //inven.AddItem(collision.gameObject);
+            
             collision.gameObject.SetActive(false);
         }
     }
